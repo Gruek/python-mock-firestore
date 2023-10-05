427d939ed72db9a9645dfc3da7b0143192906a7a
Alex Kruger <alex_k@live.com.au>
2023-10-05 16:00:24 +1100

    remove long description

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="mock-firestore",
     version="0.11.0",
     author="Matt Dowds",
     description="In-memory implementation of Google Cloud Firestore for use in tests",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/mdowds/mock-firestore",
     packages=setuptools.find_packages(),
     test_suite='',
